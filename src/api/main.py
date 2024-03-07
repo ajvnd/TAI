da04a4d980ba78207b22628761bedef827afdf63
Ahmad Javadi Nezhad <ahmadjavadi17@gmail.com>
2024-03-07 13:54:49 +0330

    add new api

--- src/api/main.py
+++ src/api/main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
+from motor.motor_asyncio import AsyncIOMotorClient
 
 app = FastAPI()
 
+MONGO_URL = "mongodb://localhost:27017"
+client = AsyncIOMotorClient(MONGO_URL)
+database = client["tai"]
+collection = database["tasks"]
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def read_root():
+    return await collection.find()
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: int, q: str = None):
+    return await collection.find_one({"_id": item_id})
+
+
+@app.get("/items/add/{item_id}")
+async def inser_item(item_id: int, q: str = None):
+    await collection.insert_one({"_id": item_id})
+
+
+@app.get("/items/delete/{item_id}")
+async def delete_item(item_id: int, q: str = None):
+    await collection.delete_one({"_id": item_id})
 
