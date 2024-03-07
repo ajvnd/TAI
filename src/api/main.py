3d391aa662a7261985cc1613a949e40765b40d3f
Ahmad Javadi Nezhad <ahmadjavadi17@gmail.com>
2024-03-07 13:56:06 +0330

    fix api routes

--- src/api/main.py
+++ src/api/main.py
@@ -13,17 +13,17 @@ async def read_root():
     return await collection.find()
 
 
-@app.get("/items/{item_id}")
+@app.get("/tasks/{item_id}")
 async def read_item(item_id: int, q: str = None):
     return await collection.find_one({"_id": item_id})
 
 
-@app.get("/items/add/{item_id}")
+@app.get("/tasks/add/{item_id}")
 async def inser_item(item_id: int, q: str = None):
     await collection.insert_one({"_id": item_id})
 
 
-@app.get("/items/delete/{item_id}")
+@app.get("/tasks/delete/{item_id}")
 async def delete_item(item_id: int, q: str = None):
     await collection.delete_one({"_id": item_id})
 
