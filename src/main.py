17dc9ad0426562f4355111fec53d95ae717415df
Ahmad Javadi Nezhad <ahmadjavadi17@gmail.com>
2024-05-27 16:02:43 +0330

    add new comments.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src import models
-from src.database import engine
-from src.router import task, user
+from src.router import user, task
+from src.models import Base, engine
 
 app = FastAPI()
 app.add_middleware(
@@ -12,6 +11,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Create the database tables if they don't already exist.
+Base.metadata.create_all(bind=engine)
+
+# Include task endpoints in the main application
 app.include_router(task.router)
+
+# Include user endpoints in the application
 app.include_router(user.router)
-models.Base.metadata.create_all(bind=engine)
