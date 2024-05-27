23ebf02fca10b61d1fcbbc19091b56de8784a301
Ahmad Javadi Nezhad <ahmadjavadi17@gmail.com>
2024-05-27 16:03:59 +0330

    refactor accessing to task repository.

--- src/router/task.py
+++ src/router/task.py
@@ -1,30 +1,26 @@
 from fastapi import Depends, status, Response, APIRouter
 from sqlalchemy.orm import Session
-from src import schemas, models
-from src import repository
-from src.database import SessionLocal
+from src import schemas
+from src.models import get_db, TaskModel
+from src.repository import TaskRepository
 
-task_repository = repository.TaskRepository()
+task_model = TaskModel
 
 router = APIRouter()
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+def get_task_repository(db: Session = Depends(get_db)):
+    return TaskRepository(db)
 
 
 @router.get("/tasks", status_code=status.HTTP_200_OK)
 def get_tasks(db: Session = Depends(get_db)):
-    return task_repository.get_tasks(db)
+    return get_task_repository(db).get_tasks()
 
 
 @router.get("/tasks/{task_id}", status_code=status.HTTP_200_OK)
 def get_task(task_id: int, response: Response, db: Session = Depends(get_db)):
-    task = task_repository.get_task(task_id, db)
+    task = get_task_repository(db).get_task(task_id)
     if not task:
         response.status_code = status.HTTP_404_NOT_FOUND
         return f"{task_id} not found"
@@ -33,21 +29,21 @@ def get_task(task_id: int, response: Response, db: Session = Depends(get_db)):
 
 @router.post("/tasks", status_code=status.HTTP_201_CREATED)
 def create_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
-    db_task = models.Task(**task.dict())
-    task_repository.create_task(db_task, db)
+    db_task = task_model(**task.dict())
+    get_task_repository(db).create_task(db_task)
 
 
 @router.put("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
 def update_task(task_id: int, task: schemas.TaskUpdate, db: Session = Depends(get_db)):
-    db_task = models.Task(**task.dict())
+    db_task = task_model(**task.dict())
     db_task.id = task_id
-    task_repository.update_task(db_task, db)
+    get_task_repository(db).update_task(db_task)
 
 
 @router.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_task(task_id: int, response: Response, db: Session = Depends(get_db)):
-    task = task_repository.get_task(task_id, db)
+    task = get_task_repository(db).get_task(task_id)
     if not task:
         response.status_code = status.HTTP_404_NOT_FOUND
         return f"{task_id} not found"
-    task_repository.delete_task(task, db)
+    get_task_repository(db).delete_task(task)
