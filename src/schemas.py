c9dba57408fe949596c64673d0735b49d59e20f7
Ahmad Javadi Nezhad <ahmadjavadi17@gmail.com>
2024-05-27 16:03:20 +0330

    add new fields to task schemas

--- src/schemas.py
+++ src/schemas.py
@@ -1,12 +1,18 @@
 from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
 
 
 class TaskCreate(BaseModel):
     title: str
+    is_completed: Optional[bool] = None
+    due_date: Optional[datetime] = None
 
 
 class TaskUpdate(BaseModel):
-    title: str
+    title: Optional[str] = None
+    is_completed: Optional[bool] = None
+    due_date: Optional[datetime] = None
 
 
 class UserCreate(BaseModel):
