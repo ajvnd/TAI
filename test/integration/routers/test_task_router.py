f67f9d7d82b4e1ae8b48c49d695097592ac3bdb5
Ahmad Javadi Nezhad <ahmadjavadi17@gmail.com>
2024-06-20 19:38:15 +0330

    many changes

--- test/integration/routers/test_task_router.py
+++ test/integration/routers/test_task_router.py
@@ -1,4 +1,4 @@
-import requests
+import requests, pytest
 from fastapi import status
 
 ENDPOINT = 'http://127.0.0.1:8000/tasks'
@@ -12,8 +12,11 @@ class TestTaskRouter:
             'is_completed': True,
         }
 
-    def test_can_create_a_task(self):
-        create_response = requests.post(ENDPOINT, json=self.task)
+    def teardown_method(self, method):
+        if method.__name__ != 'test_can_delete_a_task':
+            requests.delete(f"{ENDPOINT}/{self.task_id}")
+
+    def test_can_create_a_task(self, create_response):
         assert create_response.status_code == status.HTTP_201_CREATED
 
         task_id = int(create_response.json())
@@ -21,12 +24,9 @@ class TestTaskRouter:
         assert get_response.status_code == status.HTTP_200_OK
         assert get_response.json()['title'] == self.task['title']
 
-    def test_can_update_a_task(self):
-        create_response = requests.post(ENDPOINT, json=self.task)
-        assert create_response.status_code == status.HTTP_201_CREATED
-
-        task_id = int(create_response.json())
+    def test_can_update_a_task(self, task_id):
         self.task['title'] = 'updated'
+
         update_response = requests.put(f"{ENDPOINT}/{task_id}", json=self.task)
         assert update_response.status_code == status.HTTP_204_NO_CONTENT
 
@@ -34,13 +34,19 @@ class TestTaskRouter:
         assert get_response.status_code == status.HTTP_200_OK
         assert get_response.json()['title'] == self.task['title']
 
-    def test_can_delete_a_task(self):
-        create_response = requests.post(ENDPOINT, json=self.task)
-        assert create_response.status_code == status.HTTP_201_CREATED
-
-        task_id = int(create_response.json())
+    def test_can_delete_a_task(self, task_id):
         delete_response = requests.delete(f"{ENDPOINT}/{task_id}")
         assert delete_response.status_code == status.HTTP_204_NO_CONTENT
 
         get_response = requests.get(f"{ENDPOINT}/{task_id}")
         assert get_response.status_code == status.HTTP_404_NOT_FOUND
+
+    @pytest.fixture
+    def create_response(self):
+        create_response = requests.post(ENDPOINT, json=self.task)
+        return create_response
+
+    @pytest.fixture
+    def task_id(self, create_response):
+        self.task_id = int(create_response.json())
+        return self.task_id
